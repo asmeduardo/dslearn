84943e329a16b1d0eaf347926afc4d572d95aaf0
Eduardo Melo <eduardo.melo01992@gmail.com>
2023-03-11 14:52:30 -0300

    Implemented exceptions, validation and security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.services;
+
+import com.devsuperior.dslearnbds.model.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            logger.error("Usuário " + username + " não encontrado");
+            throw new UsernameNotFoundException("Email não existe");
+        }
+        logger.info("Usuário " + username + " encontrado");
+        return user;
+    }
+}
