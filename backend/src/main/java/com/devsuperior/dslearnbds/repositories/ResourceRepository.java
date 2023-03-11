f947f4517ad6be545446624fcabde349a71877bd
Eduardo Melo <eduardo.melo01992@gmail.com>
2023-03-11 13:14:20 -0300

    Added repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+}
