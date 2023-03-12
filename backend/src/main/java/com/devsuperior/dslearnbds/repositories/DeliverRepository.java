488678b2c7fde9f24c05ab5620b825a67d898402
Eduardo Melo <eduardo.melo01992@gmail.com>
2023-03-12 19:00:36 -0300

    Preauthorizing methods

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
 import com.devsuperior.dslearnbds.model.Course;
 import com.devsuperior.dslearnbds.model.Deliver;
 import org.springframework.data.jpa.repository.JpaRepository;
